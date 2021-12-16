ae1f78a3ee7fd12586b893ef07c08ff2ee865fe2
xubury <xubury97@gmail.com>
2021-12-16 15:00:49 +0800

    * update colorscheme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,8 +1,10 @@
 local cmd = vim.cmd
-local opt = vim.opt -- Colorscheme
+local opt = vim.opt
+local g = vim.g
 opt.termguicolors = true
 opt.background = "dark"
-vim.g.gruvbox_italic_comments = false
-vim.g.gruvbox_italic_keywords = false
+g.gruvbox_italic_comments = false
+g.gruvbox_italic_keywords = false
+g.gruvbox_flat_style = "dark"
 cmd("syntax enable")
 cmd("colorscheme gruvbox-flat")
