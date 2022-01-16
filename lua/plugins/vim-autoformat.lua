f3f87c10b7d7e86269b22934f618f5127e6effb6
xubury <xubury97@gmail.com>
2022-01-16 13:46:43 +0800

    * fix auto format

--- lua/plugins/vim-autoformat.lua
+++ lua/plugins/vim-autoformat.lua
@@ -1,15 +1,21 @@
 local u = require("utils")
 local g = vim.g
+local fn = vim.fn
 
 g.autoformat_verbosemode = 1
 g.autoformat_autoindent = 0
 g.autoformat_retab = 0
 g.autoformat_remove_trailing_spaces = 1
-g.formatdef_file_clang = '"clang-format -style=file"'
-local cpp_style = "{BasedOnStyle: Google, IndentWidth: 4, SortIncludes: false}"
-g.formatdef_my_custom_clang = string.format('"clang-format -style=\\"%s\\""', cpp_style)
-g.formatters_c = {"file_clang", "my_custom_clang"}
-g.formatters_cpp = {"file_clang", "my_custom_clang"}
+
+if fn.empty(fn.glob(".clang-format")) > 0 then
+    local cpp_style = "{BasedOnStyle: Google, IndentWidth: 4, SortIncludes: false}"
+    g.formatdef_my_custom_clang = string.format('"clang-format -style=\\"%s\\""', cpp_style)
+else
+    g.formatdef_my_custom_clang = '"clang-format -style=file"'
+end
+
+g.formatters_c = {"my_custom_clang"}
+g.formatters_cpp = {"my_custom_clang"}
 
 -- let auto_format_type_list = ['c', 'cpp', 'python', 'tex']
 -- autocmd BufWrite * if index(auto_format_type_list, &ft) >= 0 | Autoformat | endif
