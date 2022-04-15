f1420c5f35022d7c0509c0a2256ad9224f01d8d0
xubury <xubury97@gmail.com>
2022-04-15 12:34:32 +0800

    * update nvim

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,27 +2,62 @@ local u = require("utils")
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 local g = vim.g
 
-g.nvim_tree_indent_markers = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_respect_buf_cwd = 1
 
 require "nvim-tree".setup {
-    disable_netrw = false,
-    hijack_netrw = true,
-    open_on_setup = false,
-    ignore_ft_on_setup = {},
-    auto_close = false,
     auto_reload_on_write = true,
-    open_on_tab = false,
+    disable_netrw = false,
+    hide_root_folder = false,
     hijack_cursor = false,
-    update_cwd = false,
+    hijack_netrw = true,
     hijack_unnamed_buffer_when_opening = false,
+    ignore_buffer_on_setup = false,
+    open_on_setup = false,
+    open_on_setup_file = false,
+    open_on_tab = false,
+    sort_by = "name",
+    update_cwd = false,
+    view = {
+        width = 30,
+        height = 30,
+        side = "left",
+        preserve_window_proportions = false,
+        number = false,
+        relativenumber = false,
+        signcolumn = "yes",
+        mappings = {
+            custom_only = false,
+            list = {}
+        }
+    },
+    renderer = {
+        indent_markers = {
+            enable = false,
+            icons = {
+                corner = "└ ",
+                edge = "│ ",
+                none = "  "
+            }
+        }
+    },
     hijack_directories = {
         enable = true,
         auto_open = true
     },
+    update_focused_file = {
+        enable = false,
+        update_cwd = false,
+        ignore_list = {}
+    },
+    ignore_ft_on_setup = {},
+    system_open = {
+        cmd = nil,
+        args = {}
+    },
     diagnostics = {
         enable = false,
+        show_on_dirs = false,
         icons = {
             hint = "",
             info = "",
@@ -30,61 +65,48 @@ require "nvim-tree".setup {
             error = ""
         }
     },
-    update_focused_file = {
-        enable = false,
-        update_cwd = false,
-        ignore_list = {}
-    },
-    system_open = {
-        cmd = nil,
-        args = {}
-    },
     filters = {
         dotfiles = false,
-        custom = {}
+        custom = {},
+        exclude = {}
     },
     git = {
         enable = true,
         ignore = false,
-        timeout = 500
-    },
-    view = {
-        width = 30,
-        height = 30,
-        hide_root_folder = false,
-        side = "left",
-        auto_resize = false,
-        preserve_window_proportions = false,
-        mappings = {
-            custom_only = false,
-            list = {}
-        },
-        number = false,
-        relativenumber = false,
-        signcolumn = "yes"
-    },
-    trash = {
-        cmd = "trash",
-        require_confirm = true
+        timeout = 400
     },
     actions = {
+        use_system_clipboard = true,
         change_dir = {
             enable = true,
             global = false
         },
         open_file = {
             quit_on_open = false,
+            resize_window = false,
             window_picker = {
                 enable = true,
                 chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
                 exclude = {
-                    filetype = {
-                        "notify",
-                        "packer",
-                        "qf"
-                    }
+                    filetype = {"notify", "packer", "qf", "diff", "fugitive", "fugitiveblame"},
+                    buftype = {"nofile", "terminal", "help"}
                 }
             }
+        }
+    },
+    trash = {
+        cmd = "trash",
+        require_confirm = true
+    },
+    log = {
+        enable = false,
+        truncate = false,
+        types = {
+            all = false,
+            config = false,
+            copy_paste = false,
+            git = false,
+            profile = false
         }
     }
 }
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup {
-    ensure_installed = "maintained",
+    ensure_installed = {"c", "cpp", "vim", "lua", "rust", "python", "latex", "glsl", "java", "json"},
     ignore_install = {}, -- List of parsers to ignore installing
     highlight = {
         enable = true, -- false will disable the whole extension
