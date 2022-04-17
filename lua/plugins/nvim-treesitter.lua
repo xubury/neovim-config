41dd0aa9d703ea25368ad3ca75e087ab1f3c535b
xubury <xubury97@gmail.com>
2022-04-17 20:09:44 +0800

    * fix syntex highlight

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,8 @@
 require("nvim-treesitter.configs").setup {
-    ensure_installed = {"c", "cpp", "vim", "lua", "rust", "python", "latex", "glsl", "java", "json"},
+    ensure_installed = {"c", "cpp", "vim", "lua", "rust", "python", "glsl", "java", "json"},
     ignore_install = {}, -- List of parsers to ignore installing
     highlight = {
         enable = true, -- false will disable the whole extension
-        disable = {} -- list of language that will be disabled
+        disable = {"latex"} -- list of language that will be disabled
     }
 }
--- lua/settings.lua
+++ lua/settings.lua
@@ -36,6 +36,9 @@ opt.updatetime = 100
 -- Autocomplete
 opt.completeopt = "menuone,noselect"
 
+-- Syntax
+opt.conceallevel = 0
+
 -- Encoding
 opt.encoding = "utf-8"
 opt.fileencodings = "ucs-bom,utf-8,cp936,gb18030,big5,euc-jp,euc-kr,latin1"
