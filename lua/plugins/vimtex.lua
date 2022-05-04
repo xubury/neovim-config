23d478f8ae78d693f4b21e88845b75585e5258a4
xubury <xubury97@gmail.com>
2022-05-04 12:16:21 +0800

    * update vimtex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -9,31 +9,6 @@ elseif fn.executable("zathura") > 0 then
     g.vimtex_view_general_viewer = "zathura"
 end
 
-g.vimtex_compiler_latexmk_engines = {
-    ["_"] = "-xelatex",
-    ["pdflatex"] = "-pdf",
-    ["dvipdfex"] = "-pdfdvi",
-    ["lualatex"] = "-lualatex",
-    ["xelatex"] = "-xelatex",
-    ["context (pdftex)"] = "-pdf -pdflatex=texexec",
-    ["context (luatex)"] = "-pdf -pdflatex=context",
-    ["context (xetex)"] = "pdf -pdflatex=''texexec --xtx''"
-}
-
-g.vimtex_syntax_concal = {
-    ["accents"] = 1,
-    ["cites"] = 0,
-    ["fancy"] = 1,
-    ["greek"] = 1,
-    ["math_bounds"] = 0,
-    ["math_delimiters"] = 0,
-    ["math_fracs"] = 0,
-    ["math_super_sub"] = 0,
-    ["math_symbols"] = 0,
-    ["sections"] = 0,
-    ["styles"] = 1
-}
-
 g.vimtex_syntax_conceal_disable = 1
 
 cmd [[
