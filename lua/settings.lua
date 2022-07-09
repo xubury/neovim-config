5aa9ece4eaa406d80a3ca2f7d143adad9d73e114
xubury <xubury97@gmail.com>
2022-07-09 20:02:15 +0800

    * edit settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -44,7 +44,7 @@ opt.encoding = "utf-8"
 opt.fileencodings = "ucs-bom,utf-8,cp936,gb18030,big5,euc-jp,euc-kr,latin1"
 
 -- Exclusive inclusive behaviors
-opt.selection="exclusive"
+-- opt.selection="exclusive"
 
 -- File format
 if fn.has("win32") > 0 then
