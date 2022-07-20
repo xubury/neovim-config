b19b768ee26fc26c6ff744c243a142830b6df4aa
xubury <xubury97@gmail.com>
2022-07-20 18:57:13 +0800

    * fix nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,13 +2,10 @@ local u = require("utils")
 local tree_cb = require("nvim-tree.config").nvim_tree_callback
 local g = vim.g
 
-g.nvim_tree_git_hl = 1
-g.nvim_tree_respect_buf_cwd = 1
-
 require "nvim-tree".setup {
+    respect_buf_cwd = true,
     auto_reload_on_write = true,
     disable_netrw = false,
-    hide_root_folder = false,
     hijack_cursor = false,
     hijack_netrw = true,
     hijack_unnamed_buffer_when_opening = false,
@@ -32,6 +29,7 @@ require "nvim-tree".setup {
         }
     },
     renderer = {
+        highlight_git = true,
         indent_markers = {
             enable = false,
             icons = {
