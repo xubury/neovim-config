031c6186feacac84e48a28ecc70aa6effc1b4ebd
xubury <xubury97@gmail.com>
2022-08-21 14:56:06 +0800

    * fix lspconfig

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,157 +1,131 @@
-local cmp = require "cmp"
+local cmp = require("cmp")
+
 local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-local feedkey = function(key, mode)
-    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col > 1 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-cmp.setup(
-    {
-        snippet = {
-            expand = function(args)
-                -- For `vsnip` user.
-                vim.fn["vsnip#anonymous"](args.body)
-                -- For `ultisnips` user.
-                -- vim.fn["UltiSnips#Anon"](args.body)
-            end
-        },
-        -- preselect = cmp.PreselectMode.None,
-        experimental = {
-            ghost_text = true
-        },
-        completion = {
-            autocomplete = false,
-            completeopt = "menu,menuone,noinsert"
-        },
-        mapping = cmp.mapping.preset.insert(
-            {
-                ["<Tab>"] = cmp.mapping(
-                    function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                        elseif has_words_before() then
-                            cmp.complete({ select = false })
-                        else
-                            fallback()
-                        end
-                    end,
-                    { "i", "s" }
-                ),
-                ["<S-Tab>"] = cmp.mapping(
-                    function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-                        elseif has_words_before() then
-                            cmp.complete({ select = false })
-                        else
-                            fallback()
-                        end
-                    end,
-                    { "i", "s" }
-                ),
-                ["<Backspace>"] = cmp.mapping(
-                    function(fallback)
-                        if cmp.visible() and not has_words_before() then
-                            cmp.close()
-                        end
-                        fallback()
-                    end,
-                    { "i", "s" }
-                ),
-                ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.close(),
-                ["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "s", "c" }),
-                ["<C-j>"] = cmp.mapping(
-                    function()
-                        if vim.fn["vsnip#available"]() == 1 then
-                            feedkey("<Plug>(vsnip-expand-or-jump)", "")
-                        end
-                    end,
-                    { "i", "s" }
-                ),
-                ["<C-k>"] = cmp.mapping(
-                    function()
-                        if vim.fn["vsnip#jumpable"](-1) == 1 then
-                            feedkey("<Plug>(vsnip-jump-prev)", "")
-                        end
-                    end,
-                    { "i", "s" }
-                )
-            }
-        ),
-        sources = {
-            { name = "nvim_lsp" },
-            { name = "vsnip" },
-            { name = "path" },
-            { name = "buffer" }
-        }
-    }
-)
+local feedkey = function(key, mode)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			-- For `vsnip` user.
+			vim.fn["vsnip#anonymous"](args.body)
+			-- For `ultisnips` user.
+			-- vim.fn["UltiSnips#Anon"](args.body)
+		end,
+	},
+	-- preselect = cmp.PreselectMode.None,
+	experimental = {
+		ghost_text = true,
+	},
+	completion = {
+		autocomplete = false,
+		completeopt = "menu,menuone,noinsert",
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+			elseif has_words_before() then
+				cmp.complete({ select = false })
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+			elseif has_words_before() then
+				cmp.complete({ select = false })
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<Backspace>"] = cmp.mapping(function(fallback)
+			if cmp.visible() and not has_words_before() then
+				cmp.close()
+			end
+			fallback()
+		end, { "i", "s" }),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<CR>"] = cmp.mapping(cmp.mapping.confirm({ select = true }), { "i", "s", "c" }),
+		["<C-j>"] = cmp.mapping(function()
+			if vim.fn["vsnip#available"]() == 1 then
+				feedkey("<Plug>(vsnip-expand-or-jump)", "")
+			end
+		end, { "i", "s" }),
+		["<C-k>"] = cmp.mapping(function()
+			if vim.fn["vsnip#jumpable"](-1) == 1 then
+				feedkey("<Plug>(vsnip-jump-prev)", "")
+			end
+		end, { "i", "s" }),
+	}),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" },
+		{ name = "path" },
+		{ name = "buffer" },
+	},
+})
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(
-    "/",
-    {
-        mapping = {
-            ["<Tab>"] = {
-                c = function()
-                    if cmp.visible() then
-                        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                    else
-                        cmp.complete({ select = false })
-                    end
-                end
-            },
-            ["<S-Tab>"] = {
-                c = function()
-                    if cmp.visible() then
-                        cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-                    else
-                        cmp.complete({ select = false })
-                    end
-                end
-            }
-        },
-        sources = {
-            { name = "buffer" }
-        }
-    }
-)
+cmp.setup.cmdline("/", {
+	mapping = {
+		["<Tab>"] = {
+			c = function()
+				if cmp.visible() then
+					cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+				else
+					cmp.complete({ select = false })
+				end
+			end,
+		},
+		["<S-Tab>"] = {
+			c = function()
+				if cmp.visible() then
+					cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+				else
+					cmp.complete({ select = false })
+				end
+			end,
+		},
+	},
+	sources = {
+		{ name = "buffer" },
+	},
+})
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(
-    ":",
-    {
-        mapping = {
-            ["<Tab>"] = {
-                c = function()
-                    if cmp.visible() then
-                        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                    else
-                        cmp.complete({ select = false })
-                    end
-                end
-            },
-            ["<S-Tab>"] = {
-                c = function()
-                    if cmp.visible() then
-                        cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-                    else
-                        cmp.complete({ select = false })
-                    end
-                end
-            }
-        },
-        sources = cmp.config.sources(
-            {
-                { name = "path" }
-            },
-            {
-                { name = "cmdline" }
-            }
-        )
-    }
-)
+cmp.setup.cmdline(":", {
+	mapping = {
+		["<Tab>"] = {
+			c = function()
+				if cmp.visible() then
+					cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+				else
+					cmp.complete({ select = false })
+				end
+			end,
+		},
+		["<S-Tab>"] = {
+			c = function()
+				if cmp.visible() then
+					cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+				else
+					cmp.complete({ select = false })
+				end
+			end,
+		},
+	},
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+})
--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -2,145 +2,145 @@ local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 local packer_bootstrap
 if fn.empty(fn.glob(install_path)) > 0 then
-    print("cloning packer into " .. install_path)
-    packer_bootstrap =
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+	print("cloning packer into " .. install_path)
+	packer_bootstrap =
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
 end
 
 local packer = require("packer")
 -- Add packages
-return packer.startup(
-    function(use)
-        use "wbthomason/packer.nvim"
+return packer.startup(function(use)
+	use("wbthomason/packer.nvim")
 
-        -- Colorscheme
-        use "eddyekofo94/gruvbox-flat.nvim"
-        use { "catppuccin/nvim", as = "catppuccin" }
+	-- Colorscheme
+	use("eddyekofo94/gruvbox-flat.nvim")
+	use({ "catppuccin/nvim", as = "catppuccin" })
 
-        -- Neovim plugin to improve the default vim.ui interfaces
-        use "stevearc/dressing.nvim"
-        use "rcarriga/nvim-notify"
-        -- Intro screen
-        use "goolord/alpha-nvim"
-        -- Tabline plugin
-        use {
-            "romgrk/barbar.nvim",
-            requires = { "kyazdani42/nvim-web-devicons" }
-        }
-        -- File explorer
-        use {
-            "kyazdani42/nvim-tree.lua",
-            requires = {
-                "kyazdani42/nvim-web-devicons" -- optional, for file icon
-            }
-        }
+	-- Neovim plugin to improve the default vim.ui interfaces
+	use("stevearc/dressing.nvim")
+	use("rcarriga/nvim-notify")
+	-- Intro screen
+	use("goolord/alpha-nvim")
+	-- Tabline plugin
+	use({
+		"romgrk/barbar.nvim",
+		requires = { "kyazdani42/nvim-web-devicons" },
+	})
+	-- File explorer
+	use({
+		"kyazdani42/nvim-tree.lua",
+		requires = {
+			"kyazdani42/nvim-web-devicons", -- optional, for file icon
+		},
+	})
 
-        -- Better quick fix
-        use "kevinhwang91/nvim-bqf"
+	-- Better quick fix
+	use("kevinhwang91/nvim-bqf")
 
-        -- Tmux navigator
-        use "christoomey/vim-tmux-navigator"
+	-- Tmux navigator
+	use("christoomey/vim-tmux-navigator")
 
-        use "windwp/nvim-autopairs"
-        -- Delete/change/add parentheses/quotes/XML-tags/much more with ease
-        use "tpope/vim-surround"
+	use("windwp/nvim-autopairs")
+	-- Delete/change/add parentheses/quotes/XML-tags/much more with ease
+	use("tpope/vim-surround")
 
-        -- Display the indention levels with thin vertical lines
-        use "lukas-reineke/indent-blankline.nvim"
+	-- Adds indentation guides to all lines
+	use("lukas-reineke/indent-blankline.nvim")
 
-        -- Commenter
-        use "preservim/nerdcommenter"
+	-- Commenter
+	use("preservim/nerdcommenter")
 
-        -- Color preview
-        use "norcalli/nvim-colorizer.lua"
+	-- Color preview
+	use("norcalli/nvim-colorizer.lua")
 
-        -- Multi cursor
-        use "mg979/vim-visual-multi"
+	-- Multi cursor
+	use("mg979/vim-visual-multi")
 
+	-- Status line
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = {
+			"kyazdani42/nvim-web-devicons",
+			opt = true,
+		},
+	})
 
-        -- Status line
-        use {
-            "nvim-lualine/lualine.nvim",
-            requires = {
-                "kyazdani42/nvim-web-devicons",
-                opt = true
-            }
-        }
+	-- Git plugin
+	use({
+		"lewis6991/gitsigns.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+	})
+	use("tpope/vim-fugitive")
 
-        -- Git plugin
-        use {
-            "lewis6991/gitsigns.nvim",
-            requires = {
-                "nvim-lua/plenary.nvim"
-            }
-        }
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+	})
+	use({
+		"nvim-telescope/telescope-fzf-native.nvim",
+		run = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+	})
 
-        use {
-            "nvim-telescope/telescope.nvim",
-            requires = {
-                "nvim-lua/plenary.nvim"
-            }
-        }
-        use { 'nvim-telescope/telescope-fzf-native.nvim',
-            run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
 
-        use {
-            "nvim-treesitter/nvim-treesitter",
-            run = ":TSUpdate"
-        }
+	-- Clipboard manager
+	use("AckslD/nvim-neoclip.lua")
 
-        -- Clipboard manager
-        use "AckslD/nvim-neoclip.lua"
+	-- Portable package manager for Neovim that runs everywhere Neovim runs.
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("WhoIsSethDaniel/mason-tool-installer.nvim")
 
-        -- Portable package manager for Neovim that runs everywhere Neovim runs.
-        use 'williamboman/mason.nvim'
-        use "williamboman/mason-lspconfig.nvim"
-        use "WhoIsSethDaniel/mason-tool-installer.nvim"
+	-- LSP support
+	use("neovim/nvim-lspconfig")
+	use("ray-x/lsp_signature.nvim")
+	use("jose-elias-alvarez/null-ls.nvim")
 
-        -- LSP support
-        use "neovim/nvim-lspconfig"
-        use "ray-x/lsp_signature.nvim"
-        use "jose-elias-alvarez/null-ls.nvim"
+	-- LSP complete menu
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-vsnip",
+			"hrsh7th/vim-vsnip",
+			"hrsh7th/vim-vsnip-integ",
+			"rafamadriz/friendly-snippets",
+		},
+	})
 
-        -- LSP complete menu
-        use {
-            "hrsh7th/nvim-cmp",
-            requires = {
-                "hrsh7th/cmp-nvim-lsp",
-                "hrsh7th/cmp-buffer",
-                "hrsh7th/cmp-cmdline",
-                "hrsh7th/cmp-path",
-                "hrsh7th/cmp-vsnip",
-                "hrsh7th/vim-vsnip",
-                "hrsh7th/vim-vsnip-integ",
-                "rafamadriz/friendly-snippets"
-            }
-        }
+	use("simrat39/symbols-outline.nvim")
 
-        use "simrat39/symbols-outline.nvim"
+	-- Markdown preview
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && yarn install",
+	})
 
-        -- Markdown preview
-        use {
-            "iamcco/markdown-preview.nvim",
-            run = "cd app && yarn install"
-        }
+	-- Latex
+	use("lervag/vimtex")
 
-        -- Latex
-        use "lervag/vimtex"
+	-- CMake
+	use({
+		"Shatur/neovim-cmake",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+	})
 
-        -- CMake
-        use {
-            "Shatur/neovim-cmake",
-            requires = {
-                "nvim-lua/plenary.nvim",
-            }
-        }
+	-- Visual Debugger
+	use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
 
-        -- Visual Debugger
-        use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
-
-        if packer_bootstrap then
-            require("packer").sync()
-        end
-    end
-)
+	if packer_bootstrap then
+		require("packer").sync()
+	end
+end)
