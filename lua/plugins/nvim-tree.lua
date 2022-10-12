f85157414bea10268e81488c87af3b889527ea9d
xubury <xubury97@gmail.com>
2022-10-12 17:44:22 +0800

    * update

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,7 +7,6 @@ NTree.setup({
 	sort_by = "case_sensitive",
 	sync_root_with_cwd = true,
 	view = {
-		adaptive_size = true,
 		mappings = {
 			list = {
 				{ key = "u", action = "dir_up" },
@@ -15,12 +14,6 @@ NTree.setup({
 				{ key = "<cr>", action = "cd" },
 			},
 		},
-	},
-	renderer = {
-		group_empty = true,
-	},
-	filters = {
-		dotfiles = true,
 	},
 })
 
