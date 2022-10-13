68382af5ec6037a46865df1d8baca9dcc6ba1520
xubury <xubury97@gmail.com>
2022-10-13 23:44:39 +0800

    * update

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -11,3 +11,5 @@ vim.fn.sign_define('DapBreakpointCondition', { text='ﳁ', texthl='DapBreakpoint
 vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl= 'DapBreakpoint' })
 vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
 vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
+
+vim.g.kanagawa_lualine_bold = false
