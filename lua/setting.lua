d9dacece359d78f7501d932d3f8044d98671225c
xubury <xubury97@gmail.com>
2023-03-28 14:33:06 +0800

    * fix search

--- lua/setting.lua
+++ lua/setting.lua
@@ -78,3 +78,6 @@ vim.api.nvim_create_user_command("Wq", "wq", {})
 vim.api.nvim_create_user_command("W", "w", {})
 vim.api.nvim_create_user_command("Qa", "qa", {})
 vim.api.nvim_create_user_command("Q", "q", {})
+
+-- don't jump while search
+opt.incsearch = false
