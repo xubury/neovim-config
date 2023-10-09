4e42f10518dd5ab263099f01adb2ba5e66f218dc
xubury <xubury97@gmail.com>
2023-10-09 14:03:53 +0800

    * update settings

--- lua/setting.lua
+++ lua/setting.lua
@@ -78,6 +78,7 @@ vim.api.nvim_create_user_command("Wq", "wq", {})
 vim.api.nvim_create_user_command("W", "w", {})
 vim.api.nvim_create_user_command("Qa", "qa", {})
 vim.api.nvim_create_user_command("Q", "q", {})
+vim.api.nvim_create_user_command("V", "v", {})
 
 -- don't jump while search
 opt.incsearch = false
