3742e5b8232461285e1c5c3c081be6b25b016d18
xubury <xubury97@gmail.com>
2023-11-11 21:51:51 +0800

    * fix keymap

--- lua/init/nvim-hop.lua
+++ lua/init/nvim-hop.lua
@@ -1,4 +1,6 @@
-vim.keymap.set("", "<leader>e", function()
+local mode = {"n", "x"}
+
+vim.keymap.set(mode, "<leader>e", function()
     local hop = require("hop")
     local hint = require("hop.hint")
     hop.hint_words({
@@ -7,7 +9,7 @@ vim.keymap.set("", "<leader>e", function()
         hint_position = require("hop.hint").HintPosition.END,
     })
 end)
-vim.keymap.set("", "<leader>E", function()
+vim.keymap.set(mode, "<leader>E", function()
     local hint = require("hop.hint")
     require("hop").hint_words({
         direction = hint.HintDirection.AFTER_CURSOR,
@@ -16,7 +18,7 @@ vim.keymap.set("", "<leader>E", function()
     })
 end)
 
-vim.keymap.set("", "<leader>ge", function()
+vim.keymap.set(mode, "<leader>ge", function()
     local hint = require("hop.hint")
     require("hop").hint_words({
         direction = hint.HintDirection.BEFORE_CURSOR,
@@ -24,7 +26,7 @@ vim.keymap.set("", "<leader>ge", function()
         hint_position = hint.HintPosition.END,
     })
 end)
-vim.keymap.set("", "<leader>gE", function()
+vim.keymap.set(mode, "<leader>gE", function()
     local hint = require("hop.hint")
     require("hop").hint_words({
         direction = hint.HintDirection.BEFORE_CURSOR,
@@ -33,50 +35,50 @@ vim.keymap.set("", "<leader>gE", function()
     })
 end)
 
-vim.keymap.set("", "<leader>w", function()
+vim.keymap.set(mode, "<leader>w", function()
     local hint = require("hop.hint")
     require("hop").hint_words({ direction = hint.HintDirection.AFTER_CURSOR, current_line_only = true })
 end)
 
-vim.keymap.set("", "<leader>W", function()
+vim.keymap.set(mode, "<leader>W", function()
     local hint = require("hop.hint")
     require("hop").hint_words({ direction = hint.HintDirection.AFTER_CURSOR, current_line_only = false })
 end)
 
-vim.keymap.set("", "<leader>b", function()
+vim.keymap.set(mode, "<leader>b", function()
     local hint = require("hop.hint")
     require("hop").hint_words({ direction = hint.HintDirection.BEFORE_CURSOR, current_line_only = true })
 end)
 
-vim.keymap.set("", "<leader>B", function()
+vim.keymap.set(mode, "<leader>B", function()
     local hint = require("hop.hint")
     require("hop").hint_words({ direction = hint.HintDirection.BEFORE_CURSOR, current_line_only = false })
 end)
 
-vim.keymap.set("", "<leader>j", function()
+vim.keymap.set(mode, "<leader>j", function()
     local hint = require("hop.hint")
     require("hop").hint_vertical({ direction = hint.HintDirection.AFTER_CURSOR })
 end)
 
-vim.keymap.set("", "<leader>J", function()
+vim.keymap.set(mode, "<leader>J", function()
     require("hop").hint_vertical()
 end)
 
-vim.keymap.set("", "<leader>k", function()
+vim.keymap.set(mode, "<leader>k", function()
     local hint = require("hop.hint")
     require("hop").hint_vertical({ direction = hint.HintDirection.BEFORE_CURSOR })
 end)
 
-vim.keymap.set("", "<leader>gg", function()
+vim.keymap.set(mode, "<leader>gg", function()
     require("hop").hint_lines()
 end)
 
-vim.keymap.set("", "s", function()
+vim.keymap.set(mode, "s", function()
     local hint = require("hop.hint")
     require("hop").hint_patterns({ direction = hint.HintDirection.AFTER_CURSOR, current_line_only = false })
 end)
 
-vim.keymap.set("", "<leader>s", function()
+vim.keymap.set(mode, "<leader>s", function()
     local hint = require("hop.hint")
     require("hop").hint_patterns({ direction = hint.HintDirection.BEFORE_CURSOR, current_line_only = false })
 end)
