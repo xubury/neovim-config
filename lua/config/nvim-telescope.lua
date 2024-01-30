e6b797f7db3b8cff9d24e6a7e5b4398fd0878e5f
buryxu <buryxu@tencent.com>
2024-01-30 11:27:04 +0800

    fix telescope

--- lua/config/nvim-telescope.lua
+++ lua/config/nvim-telescope.lua
@@ -1,5 +1,7 @@
 local telescope = require("telescope")
 local actions = require("telescope.actions")
+local actions_state = require("telescope.actions.state")
+local project_actions = require("telescope._extensions.project.actions")
 telescope.setup({
     extensions = {
         fzf = {
@@ -8,6 +10,14 @@ telescope.setup({
             override_file_sorter = true, -- override the file sorter
             case_mode = "smart_case", -- or "ignore_case" or "respect_case"
             -- the default case_mode is "smart_case"
+        },
+        project = {
+            on_project_selected = function(prompt_bufnr)
+                local entry = actions_state.get_selected_entry(prompt_bufnr)
+                if entry then
+                    project_actions.change_working_directory(prompt_bufnr, false)
+                end
+            end,
         },
     },
     defaults = {
