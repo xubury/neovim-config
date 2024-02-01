d551d4f087b746ba26a3bf889877d80aecef33c8
xubury <xubury97@gmail.com>
2024-02-01 22:25:44 +0800

    toggle quickfix

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -1,5 +1,22 @@
 local M = {}
+
 M.setup = function()
+    local toggle_qf = function()
+        local qf_exists = false
+        for _, win in pairs(vim.fn.getwininfo()) do
+            if win["quickfix"] == 1 then
+                qf_exists = true
+            end
+        end
+        if qf_exists == true then
+            vim.cmd("cclose")
+            return
+        end
+        if not vim.tbl_isempty(vim.fn.getqflist()) then
+            vim.cmd("copen")
+        end
+    end
+
     vim.keymap.set("v", "Y", "y")
     -- CTRL-X Cut
     vim.keymap.set("v", "<C-X>", '"+x')
@@ -39,6 +56,8 @@ M.setup = function()
     vim.keymap.set("", "<A-9>", "9gt")
     -- Last tab
     vim.keymap.set("", "<A-0>", "<cmd>tablast<cr>")
+    vim.keymap.set("", "<A-W>", "<cmd>tabclose<cr>")
+    vim.keymap.set("", "<leader>c", toggle_qf)
 
     -- Last buffer
     vim.keymap.set("", "<leader><tab>", "<cmd>b# <cr>")
