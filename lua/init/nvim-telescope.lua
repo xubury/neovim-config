57e9aa1da1156b1f8b90287ccb7ccd2a03a5b141
xubury <xubury97@gmail.com>
2024-03-29 22:26:25 +0800

    Update telescope

--- lua/init/nvim-telescope.lua
+++ lua/init/nvim-telescope.lua
@@ -1,14 +1,11 @@
-local telescope_project_files = function()
-    local builtin = require("telescope.builtin")
-    local opts = { show_untracked = true } -- define here if you want to define something
-    local ok = pcall(builtin.git_files, opts)
-    if not ok then
-        builtin.find_files()
-    end
-end
-local telescope_project = function()
-    require("telescope").extensions.project.project({})
-end
+-- local telescope_project_files = function()
+--     local builtin = require("telescope.builtin")
+--     local opts = { show_untracked = true } -- define here if you want to define something
+--     local ok = pcall(builtin.git_files, opts)
+--     if not ok then
+--         builtin.find_files()
+--     end
+-- end
 
 vim.keymap.set("n", "<C-f>", function()
     require("telescope.builtin").current_buffer_fuzzy_find()
@@ -16,8 +13,12 @@ end)
 vim.keymap.set("n", "<leader>fw", function()
     require("telescope.builtin").live_grep()
 end)
-vim.keymap.set("n", "<leader>ff", telescope_project_files)
-vim.keymap.set("n", "<leader>fp", telescope_project)
+vim.keymap.set("n", "<leader>ff", function()
+    require("telescope.builtin").find_files()
+end)
+vim.keymap.set("n", "<leader>fp", function()
+    require("telescope").extensions.project.project({})
+end)
 vim.keymap.set("n", "<leader>fb", function()
     require("telescope.builtin").buffers()
 end)
