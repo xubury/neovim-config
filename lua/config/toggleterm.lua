d64e90bc3efaf54be4d5ae05fc106a1398b55da6
xubury <xubury97@gmail.com>
2024-03-31 13:13:48 +0800

    Fix config

--- lua/config/toggleterm.lua
+++ lua/config/toggleterm.lua
@@ -3,11 +3,3 @@ vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>")
 require("toggleterm").setup({
     shell = "pwsh",
 })
-
-vim.cmd([[
-		let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-		let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';'
-		let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-		let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
-		set shellquote= shellxquote=
-]])
--- lua/plugin.lua
+++ lua/plugin.lua
@@ -108,6 +108,9 @@ local plugins = {
     {
         "kylechui/nvim-surround",
         version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({})
+        end,
         event = "VeryLazy",
     },
 
