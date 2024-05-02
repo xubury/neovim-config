c21ff0142424d94c2ec4fac638355c27561a038d
xubury <xubury97@gmail.com>
2024-05-02 22:03:43 +0800

    update shell

--- lua/setting.lua
+++ lua/setting.lua
@@ -81,12 +81,13 @@ opt.incsearch = false
 
 vim.opt.shortmess = "filnxtToOFIW"
 
-if vim.fn.has("win32") then
-    vim.cmd([[
-		let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
-		let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';'
-		let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
-		let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
-		set shellquote= shellxquote=
-]])
-end
+-- TODO: Unreal Plugin can't handle powershell
+-- if vim.fn.has("win32") then
+--     vim.cmd([[
+--         let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+--         let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';'
+--         let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+--         let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+--         set shellquote= shellxquote=
+-- ]])
+-- end
