8cc22e67e6741865743dffc67d782df8e5ada50e
xubury <xubury97@gmail.com>
2024-06-03 09:53:51 +0800

    Update lsp

--- lua/config/nvim-lspconfig.lua
+++ lua/config/nvim-lspconfig.lua
@@ -17,33 +17,20 @@ end
 lsp_setup("vimls") -- Vim
 
 -- Lua
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
 lsp_setup("lua_ls", {
     root_dir = root_pattern(".luarc.json", ".luarc.jsonc", ".luacheckrc", "selene.toml", "selene.yml", ".git"),
     settings = {
         Lua = {
             runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = "LuaJIT",
-                -- Setup your lua path
-                path = runtime_path,
             },
             diagnostics = {
-                -- Get the language server to recognize the `vim` global
                 globals = { "vim" },
             },
             workspace = {
-                -- Make the server aware of Neovim runtime files
-                -- library = vim.api.nvim_get_runtime_file("", true),
                 checkThirdParty = false, -- THIS IS THE IMPORTANT LINE TO ADD
             },
             semantic = {
-                enable = false,
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
                 enable = false,
             },
         },
