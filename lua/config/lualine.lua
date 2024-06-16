b06e5629604257c3f85db10824d3c3216febfc88
xubury <xubury97@gmail.com>
2024-06-16 16:36:57 +0800

    Update lualine

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -1,48 +1,18 @@
 local palette = require("catppuccin.palettes").get_palette()
 
-local colors = {
-    blue = palette.blue,
-    cyan = palette.sky,
-    black = "#080808",
-    white = palette.text,
-    red = palette.red,
-    violet = palette.mauve,
-    base = palette.base,
-    overlay = palette.surface0,
-}
-
-local bubbles_theme = {
-    normal = {
-        a = { fg = colors.black, bg = colors.violet },
-        b = {},
-        c = {},
-    },
-    insert = { a = { fg = colors.black, bg = colors.blue } },
-    visual = { a = { fg = colors.black, bg = colors.cyan } },
-    replace = { a = { fg = colors.black, bg = colors.red } },
-    terminal = { a = { fg = colors.black, bg = colors.red } },
-    inactive = {
-        a = { fg = colors.white, bg = colors.base },
-    },
-}
-
 require("lualine").setup({
     options = {
         theme = "catppuccin",
-        component_separators = "|",
-        section_separators = { left = "", right = "" },
+        component_separators = "",
+        section_separators = "",
     },
     sections = {
-        lualine_a = {
-            { "mode", separator = { left = "" }, right_padding = 2 },
-        },
+        lualine_a = { "mode" },
         lualine_b = { "filename" },
         lualine_c = {},
         lualine_x = {},
         lualine_y = { "fileformat", "filetype", "progress" },
-        lualine_z = {
-            { "location", separator = { right = "" }, left_padding = 2 },
-        },
+        lualine_z = { "location" },
     },
     inactive_sections = {
         lualine_a = { "filename" },
