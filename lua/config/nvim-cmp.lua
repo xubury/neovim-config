001652d358ea3c7c57715d4e9a23c5644a9034b6
xubury <xubury97@gmail.com>
2026-04-21 19:36:00 +0800

    udpate

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -81,7 +81,14 @@ cmp.setup({
         { name = "nvim_lsp" },
         { name = "vsnip" },
         { name = "path" },
-        { name = "buffer" },
+        {
+            name = "buffer",
+            option = {
+                get_bufnrs = function()
+                    return vim.api.nvim_list_bufs()
+                end,
+            },
+        },
     },
 })
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
